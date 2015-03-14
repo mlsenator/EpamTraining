57b48394f38547816cf08654a6b81ba40ef90af2
mlsenator <andrew.shukailau@gmail.com>
2015-03-14 19:29:46 +0300

    Day2 commit

---
+++ Day2/UnitTestGCD/GCDTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathHelper;
+
+namespace UnitTestGCD
+{
+    [TestClass]
+    public class GCDTests
+    {
+        [TestMethod]
+        public void EvklidMethodTest()
+        {
+            GreatestCommonDivisor gcd = new GreatestCommonDivisor();
+            int a = 40;
+            int b = 37;
+            int expected = 1;
+            Assert.AreEqual(expected, gcd.Evklid(a, b));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EvklidMethodExceptionTest()
+        {
+            GreatestCommonDivisor gcd = new GreatestCommonDivisor();
+            int a = 4;
+            int b = -1;
+            int actual = gcd.Evklid(a, b);
+        }
+
+        [TestMethod]
+        public void EvklidParamsMethodTest()
+        {
+            GreatestCommonDivisor gcd = new GreatestCommonDivisor();
+            int expected = 7;
+            Assert.AreEqual(expected, gcd.Evklid(70, 21, 14, 7));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EvklidParamsMethodExceptionTest()
+        {
+            GreatestCommonDivisor gcd = new GreatestCommonDivisor();
+            int actual = gcd.Evklid(43, 34, -1);
+        }
+
+        [TestMethod]
+        public void EvklidMethodZeroTest()
+        {
+            GreatestCommonDivisor gcd = new GreatestCommonDivisor();
+            int expected = 7;
+            Assert.AreEqual(expected, gcd.Evklid(0, 70, 14, 7));
+        }
+
+        [TestMethod]
+        public void SteinMethodTest()
+        {
+            GreatestCommonDivisor gcd = new GreatestCommonDivisor();
+            int a = 40;
+            int b = 37;
+            int expected = gcd.Evklid(a, b);
+            Assert.AreEqual(expected, gcd.Stein(a, b));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SteinMethodExceptionTest()
+        {
+            GreatestCommonDivisor gcd = new GreatestCommonDivisor();
+            int a = 4;
+            int b = -1;
+            int actual = gcd.Stein(a, b);
+        }
+
+        [TestMethod]
+        public void SteinParamsMethodTest()
+        {
+            GreatestCommonDivisor gcd = new GreatestCommonDivisor();
+            int expected = gcd.Evklid(70, 21, 14, 7);
+            Assert.AreEqual(expected, gcd.Stein(70, 21, 14, 7));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SteinParamsMethodExceptionTest()
+        {
+            GreatestCommonDivisor gcd = new GreatestCommonDivisor();
+            int actual = gcd.Stein(432, 234, -1);
+        }
+
+        [TestMethod]
+        public void SteinMethodZeroTest()
+        {
+            GreatestCommonDivisor gcd = new GreatestCommonDivisor();
+            int expected = gcd.Evklid(0, 70, 14, 7);
+            Assert.AreEqual(expected, gcd.Stein(0, 70, 14, 7));
+        }
+
+    }
+}
