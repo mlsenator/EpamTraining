2ad770161413f0e4e1010a84406ddb99c8eaa8d8
mlsenator <andrew.shukailau@gmail.com>
2015-03-25 02:36:30 +0300

    Day 8 initial

---
+++ Day8/CollectionExtension/CollectionExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionExtension
+{
+    public static class CollectionExtension
+    {
+        /// <summary>
+        /// Returns custom enumerator
+        /// </summary>
+        /// <param name="itemsFilter">Specific items selection rule. Type null to use default (include all items).</param>
+        /// <param name="indexesFilter">Specific indexes selection rule. Type null to use default (include all indexes).</param>
+        public static IEnumerable<T> CustomEnumerator<T>(this IEnumerable<T> collection, Func<T, bool> itemsFilter, Func<int, bool> indexesFilter)
+        {
+            if (collection == null)
+                throw new NullReferenceException("Collection is null");
+            if (itemsFilter == null)
+                itemsFilter = (T param) => true;
+            if (indexesFilter == null)
+                indexesFilter = (int param) => true;
+            int i = 0;
+            foreach (T item in collection)
+            {
+                if (indexesFilter(i))
+                    if (itemsFilter(item))
+                        yield return item;
+                i++;
+            }
+        }
+
+    }
+}
---
+++ Day8/ConsoleUI/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CollectionExtension;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] collection1 = {0,1,2,3,4,5,6,7,8,9};
+            string[] collection2 = { "Zero", "One", "Two", "Three", "Four", "Five" };
+
+            foreach (var item in collection1.CustomEnumerator(null, null))
+                Console.Write(item + "  ");
+            Console.WriteLine("\n");
+
+            foreach (var item in collection1.CustomEnumerator(null, x => (x % 2 == 0)))
+                Console.Write(item + "  ");
+            Console.WriteLine("\n");
+
+            foreach (var item in collection1.CustomEnumerator(x => x > 5, null))
+                Console.Write(item + "  ");
+            Console.WriteLine("\n");
+
+            foreach (var item in collection2)
+                Console.Write(item + "  ");
+            Console.WriteLine("\n");
+
+            foreach (var item in collection2.CustomEnumerator(x => x[0] == 'F', null))
+                Console.Write(item + "  ");
+            Console.WriteLine("\n");
+
+            foreach (var item in collection2.CustomEnumerator(x => x.Length > 3, null))
+                Console.Write(item + "  ");
+            Console.WriteLine("\n");
+
+            Console.ReadLine();
+        }
+    }
+}
